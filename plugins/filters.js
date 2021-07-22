d98bcb9be386530fcb99ef48244244ceb2bdb7ae
misirile-rajathi <87692212+misirile-rajathi@users.noreply.github.com>
2021-07-22 17:47:54 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -147,7 +147,9 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     if (!!message.mention && message.mention[0] == '918547134314@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/mention.mp3'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
     }
-    const array = ['Paathu','Mm','sed','mention','paathu','welcome','ചോദിക്കും','poda myre','eid mubarak','mess','umma','zatan','ambada','mathi','Hi']
+    const array = [
+const array = ['Hi','Bye','Muthe','Police','Teach','Thech','Z','aayo','alla','anthas','ayin','aysheri','bie','bye','chathu','cheyalle','chunk','committed','mama','marichu','mention','mood','muthe','myre','njan','number','ok','oombi','ooo','pedicho','pidi','poweresh','sad','saved','sed','shaad','shut','teach','test','thech','think','thund','umma','uyir','vannu','vibe','z','dead','Like','pever','sry','night','indo','uff','eh','poyi','scene','killadi','nee alle','sheri','vada','poocha','morning','pm','thund','remove','Sed','araa','Da','madthu','Hlo','air','Bomb','Julie','myr','fan','charge',]
+array.map( async (a) => {
     array.map( async (a) => {
     let pattern = new RegExp(`\\b${a}\\b`, 'g');
     if(pattern.test(message.message)){
